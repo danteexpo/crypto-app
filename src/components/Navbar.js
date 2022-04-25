0e257dbc0209838055f231ca739f7800290714d5
dann3223 <dante0im@gmail.com>
2022-04-24 22:04:59 -0300

    fixed mobile screen

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,20 @@
-import React from 'react'
-import {FaCoins} from 'react-icons/fa'
-import {Link} from 'react-router-dom'
-import './Navbar.css'
+import React from "react";
+import { FaCoins } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
-    return (
-        <Link to='/'>
-            <div className='navbar'>
-                <FaCoins className='icon' />
-                <h1> Crypto <span className='purple'>World</span></h1>
-            </div>
-        </Link>
-    )
-}
+	return (
+		<Link to="/">
+			<div className="navbar">
+				<FaCoins className="icon" />
+				<h1>
+					{" "}
+					Crypto <span className="purple">World</span>
+				</h1>
+			</div>
+		</Link>
+	);
+};
 
-export default Navbar
+export default Navbar;
